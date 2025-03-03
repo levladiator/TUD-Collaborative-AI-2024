916618bf643a6357c8cb789ec0b68469f351fcbc
Vlad-Ioan Dragutoiu <v.dragutoiu-1@student.tudelft.nl>
2025-03-03 18:10:39 +0200

    fix bugs

--- agents1/AgentUtils.py
+++ agents1/AgentUtils.py
@@ -1,5 +1,12 @@
+import logging
+
 def compute_collected_adjustments(victim_name: str, base_adjustment: float):
     if "mildly" in victim_name:
         return base_adjustment, base_adjustment
     elif "critically" in victim_name:
         return base_adjustment * 2, base_adjustment * 2
+
+def log_info(use: bool, message: str):
+    logging.basicConfig(level=logging.INFO)
+    if use:
+        logging.info(message)
